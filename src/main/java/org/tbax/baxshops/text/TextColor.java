fb08004dc79c76f6445a6bb438da22b69ecf024f
Tim Baxendale <pcluddite@outlook.com>
2026-09-15 20:48:04 -0500

    Add getHexColor() to TextColor

--- src/main/java/org/tbax/baxshops/text/TextColor.java
+++ src/main/java/org/tbax/baxshops/text/TextColor.java
@@ -22,35 +22,42 @@ import org.bukkit.ChatColor;
 
 public enum TextColor
 {
-    BLACK("black", ChatColor.BLACK),
-    DARK_BLUE("dark_blue", ChatColor.DARK_BLUE),
-    DARK_GREEN("dark_green", ChatColor.DARK_GREEN),
-    DARK_AQUA("dark_aqua", ChatColor.DARK_AQUA),
-    DARK_RED("dark_red", ChatColor.DARK_RED),
-    DARK_PURPLE("dark_purple", ChatColor.DARK_PURPLE),
-    GOLD("gold", ChatColor.GOLD),
-    GRAY("gray", ChatColor.GRAY),
-    DARK_GRAY("dark_gray", ChatColor.DARK_GRAY),
-    BLUE("blue", ChatColor.BLUE),
-    GREEN("green", ChatColor.GREEN),
-    AQUA("aqua", ChatColor.AQUA),
-    RED("red", ChatColor.RED),
-    LIGHT_PURPLE("light_purple", ChatColor.LIGHT_PURPLE),
-    YELLOW("yellow", ChatColor.YELLOW),
-    WHITE("white", ChatColor.WHITE);
+    BLACK       ("black"       , ChatColor.BLACK       , 0x000000),
+    DARK_BLUE   ("dark_blue"   , ChatColor.DARK_BLUE   , 0x0000AA),
+    DARK_GREEN  ("dark_green"  , ChatColor.DARK_GREEN  , 0x00AA00),
+    DARK_AQUA   ("dark_aqua"   , ChatColor.DARK_AQUA   , 0x00AAAA),
+    DARK_RED    ("dark_red"    , ChatColor.DARK_RED    , 0xAA0000),
+    DARK_PURPLE ("dark_purple" , ChatColor.DARK_PURPLE , 0xAA00AA),
+    GOLD        ("gold"        , ChatColor.GOLD        , 0xFFAA00),
+    GRAY        ("gray"        , ChatColor.GRAY        , 0xAAAAAA),
+    DARK_GRAY   ("dark_gray"   , ChatColor.DARK_GRAY   , 0x555555),
+    BLUE        ("blue"        , ChatColor.BLUE        , 0x5555FF),
+    GREEN       ("green"       , ChatColor.GREEN       , 0x55FF55),
+    AQUA        ("aqua"        , ChatColor.AQUA        , 0x55FFFF),
+    RED         ("red"         , ChatColor.RED         , 0xFF5555),
+    LIGHT_PURPLE("light_purple", ChatColor.LIGHT_PURPLE, 0xFF55FF),
+    YELLOW      ("yellow"      , ChatColor.YELLOW      , 0xFFFF55),
+    WHITE       ("white"       , ChatColor.WHITE       , 0xFFFFFF);
 
-    private String value;
-    private ChatColor chatColor;
+    private final String value;
+    private final ChatColor chatColor;
+    private final int hexColor;
 
-    TextColor(String value, ChatColor chatColor)
+    TextColor(String value, ChatColor chatColor, int hexColor)
     {
         this.value = value;
         this.chatColor = chatColor;
+        this.hexColor = hexColor;
     }
 
     public ChatColor getChatColor()
     {
         return chatColor;
+    }
+
+    public int getHexColor()
+    {
+        return hexColor;
     }
 
     @Override
