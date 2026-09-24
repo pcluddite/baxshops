6130c1c54e8e5790ac0ce167db73bed0b8da8536
Tim Baxendale <pcluddite@outlook.com>
2026-09-24 16:12:46 -0500

    Update CmdInfo with kyori adventure text

--- src/main/java/org/tbax/baxshops/commands/CmdInfo.java
+++ src/main/java/org/tbax/baxshops/commands/CmdInfo.java
@@ -18,21 +18,25 @@
  */
 package org.tbax.baxshops.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.tbax.baxshops.BaxEntry;
+import org.tbax.baxshops.FormatText;
+import org.tbax.baxshops.Permissions;
+import org.tbax.baxshops.items.EnchantMap;
 import org.tbax.bukkit.CommandHelp;
 import org.tbax.bukkit.CommandHelpArgument;
-import org.tbax.baxshops.Format;
 import org.tbax.bukkit.commands.CmdActor;
 import org.tbax.bukkit.commands.CommandArgument;
 import org.tbax.bukkit.errors.PrematureAbortException;
-import org.tbax.baxshops.Permissions;
-import org.tbax.baxshops.items.EnchantMap;
-import org.tbax.baxshops.text.*;
 
 import java.util.List;
 import java.util.Map;
@@ -98,50 +102,65 @@ public final class CmdInfo extends ShopCommand
     {
         BaxEntry entry = actor.getArg(1).asEntry();
         int index = actor.getShop().indexOf(entry) + 1;
-        ChatComponent info = ChatComponent.of(Format.header("Entry Information"));
-        info.append("\nName: ").append(ChatComponent.of(entry.getName())
-            .hoverEvent(HoverEvent.showItem(entry.getItemStack()))
-        );
-        info.append("\nMaterial: ").append(entry.getType().toString());
+        Component info = Component.text("Name: ", NamedTextColor.WHITE)
+                .append(Component.text(entry.getName()).hoverEvent(entry.getItemStack().asHoverEvent()));
+        info = info.appendNewline()
+                .append(Component.text("Material: " + entry.getType().toString()));
         if (entry.getType().getMaxDurability() > 0) {
-            info.append("\nDamage: ").append(entry.getDamagePercent() + "%", TextColor.YELLOW);
+            info = info.appendNewline()
+                    .append(Component.text("Durability: "))
+                    .append(Component.text(entry.getDamagePercent() + "%", NamedTextColor.YELLOW));
         }
         if (entry.getItemStack().hasItemMeta()) {
             ItemMeta meta = entry.getItemStack().getItemMeta();
             if (meta.hasDisplayName()) {
-                info.append("\nDisplay Name: ").append(meta.getDisplayName(), TextColor.YELLOW);
+                info = info.appendNewline()
+                        .append(Component.text("Display Name: "))
+                        .append(meta.displayName().color(NamedTextColor.YELLOW));
             }
             if (meta.hasLore()) {
-                info.append("\nDescription:");
-                for (String line : meta.getLore()) {
-                    info.append("\n" + line, TextColor.BLUE);
+                info = info.appendNewline()
+                        .append(Component.text("Description: "));
+                for (Component line : meta.lore()) {
+                    info = info.appendNewline().append(line.color(NamedTextColor.BLUE));
                 }
             }
         }
         Map<Enchantment, Integer> enchmap = EnchantMap.getEnchants(entry.getItemStack());
         if (enchmap != null && !enchmap.isEmpty()) {
-            info.append("\nEnchants: ").append(Format.enchantments(EnchantMap.fullListString(enchmap)));
+            info = info.appendNewline()
+                    .append(Component.text("Enchants: "))
+                    .append(FormatText.enchantments(EnchantMap.fullListString(enchmap)));
         }
-        info.append("\nQuantity: ").append(entry.getAmount() == 0 ? ChatColor.DARK_RED + "OUT OF STOCK" : Format.number(entry.getAmount()));
-        info.append("\n");
+        info = info.appendNewline()
+                .append(Component.text("Quantity: "));
+        if (entry.getAmount() < 1) {
+            info = info.append(Component.text("OUT OF STOCK", NamedTextColor.DARK_RED));
+        } else {
+            info = info.append(FormatText.number(entry.getAmount()));
+        }
+        info = info.appendNewline();
         if (entry.canBuy()) {
-            info.append(ChatComponent.of("[BUY]", TextColor.GREEN, ChatTextStyle.UNDERLINED)
-                    .hoverEvent(HoverEvent.showText("Buy for " + entry.getFormattedBuyPrice()))
+            info = info.append(Component.text("[BUY]", NamedTextColor.GREEN, TextDecoration.UNDERLINED)
+                    .hoverEvent(HoverEvent.showText(Component.text("Buy for " + entry.getFormattedBuyPrice())))
                     .clickEvent(ClickEvent.suggestCommand("/buy " + index + " "))
             );
-            info.append(" ");
+            info = info.appendSpace();
         }
         if (entry.canSell()) {
-            info.append(ChatComponent.of("[SELL]", TextColor.BLUE, ChatTextStyle.UNDERLINED)
-                    .hoverEvent(HoverEvent.showText("Sell for " + entry.getFormattedSellPrice()))
+            info = info.append(Component.text("[SELL]", NamedTextColor.BLUE, TextDecoration.UNDERLINED)
+                    .hoverEvent(HoverEvent.showText(Component.text("Sell for " + entry.getFormattedSellPrice())))
                     .clickEvent(ClickEvent.suggestCommand("/shop sellfrominventory " + index + " "))
             );
         }
+        info = FormatText.header("Entry Information")
+                .appendNewline()
+                .append(info);
         if (actor.getPlayer() == null) {
-            info.sendTo(actor.getSender());
+            actor.getSender().sendMessage(info);
         }
         else {
-            info.sendTo(actor.getPlayer());
+            actor.getPlayer().sendMessage(info);
         }
     }
 
